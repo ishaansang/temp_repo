b929c5dffdc5c83721ba009b2365abbd2f8c0de6
ishaansang <ishaansang5@gmail.com>
2016-01-10 22:04:56 +0530

    File added initial commit

---
+++ temp.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define INF 1000000007
+ 
+#define all(x) (x).begin(), (x).end()
+#define nall(x) (x).rbegin(), (x).rend()
+#define tr(x,it) for(auto it = (x).begin();it!=(x).end();++it)
+#define ntr(x,it) for(auto it = (x).rbegin();it!=(x).rend();++it)
+#define sz(a) int((a).size()) 
+#define pb push_back 
+#define PB pop_back
+#define pf push_front
+#define PF pop_front
+#define MP make_pair
+#define clr clear
+#define rz resize
+#define mset(a,b) memset(a,b,sizeof(a))
+#define ia(a,n) FOR(i,0,n-1)cin>>a[i]
+#define ia1(a,n) FOR(i,1,n)cin>>a[i]
+#define fpresent(c,x) ((c).find(x) != (c).end())  // set,map
+#define present(c,x) (find(all(c),x) != (c).end())  //vector
+#define F first
+#define S second
+#define FOR(i,a,b) for(int i=a;i<=b;++i)
+#define NFOR(i,a,b) for(int i=a;i>=b;--i)
+#define rep(i,n) FOR(i,0,n-1)
+#define TCASE int __T;cin>>__T;FOR(Tc,1,__T)
+/// debug //////
+#define pr(x,n) {rep(i,n)cout<<x[i]<<" ";cout<<endl;}
+#define pr1(x,n) {FOR(i,1,n)cout<<x[i]<<" ";cout<<endl;}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+   return 0;
+}  
